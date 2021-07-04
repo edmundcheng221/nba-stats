3a0f1f7ff775032ad913a85f7d155b78505f45ef
edmundcheng221 <ec3219@nyu.edu>
2021-07-04 12:15:41 -0400

    finished Navbar component

--- nba-stats/src/components/Navbar.js
+++ nba-stats/src/components/Navbar.js
@@ -1,16 +1,35 @@
 import React, {Component} from 'react';
+import logo from '../assets/logo-sj.png';
+
 
 class Navbar extends Component{
+    constructor(props) {
+      super(props);
+      this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+      var x = document.getElementById("listMenu");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
     render() {
         return (
-            <div>
+            <div id="navbar">
               <ul id="nav">
-                <li><a href="#">Players</a></li>
-                <li><a href="#">Teams</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">About</a></li>
+                <div id="listMenu">
+                  <li><a href="#">Players</a></li>
+                  <li><a href="#">Teams</a></li>
+                  <li><a href="#">Contact</a></li>
+                  <li><a href="#">About</a></li>
+                </div>
+                <a href="javascript:void(0);" class="icon" onClick={this.handleClick}>
+                  <i class="fa fa-bars"></i>
+                </a>
                 <div id="logo-main">
-                  <li><a href="#">NBA STATS</a></li>
+                  <li><a href="#"><img src={logo} id="logo-main"></img></a></li>
                 </div>
               </ul>
             </div>
